7ce68a42b33d4c318d42c661d1ebc67cbc3c0010
FishGoddess <1149062639@qq.com>
2018-03-21 10:30:43 +0800

    第六次大更新：整体架构都被更新了！

--- src/com/fish/EasyMysql/DBManager.java
+++ src/com/fish/EasyMysql/DBManager.java
@@ -1,5 +1,7 @@
 package com.fish.EasyMysql;
 
+import core.MySQL;
+
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,11 +9,14 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 import java.util.Properties;
 
 /**
  * 管理 MySql 数据库的连接信息
- * some infomation about database's connection
+ *
+ * (some infomation about database's connection)
+ *
  * @author Fish
  * */
 public final class DBManager
@@ -20,6 +25,7 @@ public final class DBManager
      * 数据库连接配置类 config class
      * 配置文件
      * 一开始就把数据库连接配置文件加载到 config 文件中，提高效率
+     *
      * @author Fish
      * */
     private static class Config
@@ -60,30 +66,42 @@ public final class DBManager
 
     /**
      * 标记是否已经初始化，除非调用 update 方法，否则该变量不应该被改写
-     * mark the status of initation
+     *
+     * (mark the status of initation)
      * */
     private static boolean isInited = false;
 
     /**
      * 配置文件所在路径
-     * where the config file hides?
+     *
+     * (where the config file hides?)
      * */
     private static File filePath = null;
 
     /**
+     * 是否自动提交
+     *
+     * (if auto commit)
+     * */
+    private static boolean autoCommit = true;
+
+    /**
      * 不需要存在这个类的对象，只需要调用所需的方法即可
-     * single dog, single dog, single all the way! Which song you are reminded of?
+     *
+     * (single dog, single dog, single all the way! Which song you are reminded of?)
      * */
     private DBManager()
     {}
 
     /**
      * 初始化数据库管理器
-     * init this guy
-     * @param path 指定配置文件的路径 the home of config file
-     * @return true 初始化成功 good job!， false 初始化失败 you failed!
+     *
+     * (init this guy)
+     *
+     * @param path 指定配置文件的路径 (the home of config file)
+     * @return true 初始化成功 (good job!)， false 初始化失败 (you failed!)
      * */
-    private static boolean init(File path)
+    public static boolean init(File path)
     {
         if (!isInited)
         {
@@ -118,11 +136,13 @@ public final class DBManager
 
     /**
      * 根据指定文件更新数据库管理器的配置信息
-     * update connection depending on what config you give
      * 在调用该方法之前，建议先调用 DBWorker.sleep() 释放资源！
-     * Maybe you should invoke DBWorker.sleep() before invoking this method!
-     * @param path 指定配置文件的路径
-     * @return true 更新成功， false 更新失败
+     *
+     * (update connection depending on what config you give)
+     * (Maybe you should invoke DBWorker.sleep() before invoking this method!)
+     *
+     * @param path 指定配置文件的路径 (the home of config file)
+     * @return true 更新成功 (Done!)， false 更新失败 (you failed!)
      * */
     public static boolean update(File path)
     {
@@ -133,12 +153,13 @@ public final class DBManager
     }
 
     /**
-     * 更新数据库管理器的配置信息
-     * update connection using the old config file
-     * 按照第一次初始化的路径进行更新
+     * 更新数据库管理器的配置信息，按照第一次初始化的路径进行更新
      * 在调用该方法之前，建议先调用 DBWorker.sleep() 释放资源！
-     * Maybe you should invoke DBWorker.sleep() before invoking this method!
-     * @return true 更新成功， false 更新失败
+     *
+     * (update connection using the old config file)
+     * (Maybe you should invoke DBWorker.sleep() before invoking this method!)
+     *
+     * @return true 更新成功 (Done!)， false 更新失败 (you failed!)
      */
     public static boolean update()
     {
@@ -148,9 +169,11 @@ public final class DBManager
     }
 
     /**
-     * 获取数据库连接, 不推荐使用
-     * get a connection of database, don't advise!
-     * @return 返回得到的数据库连接 you get a connection
+     * 获取数据库连接, 不推荐外界直接使用
+     *
+     * (get a connection of database, don't advise!)
+     *
+     * @return 返回得到的数据库连接 (you get a connection)
      * */
     public static Connection getConnection()
     {
@@ -161,7 +184,10 @@ public final class DBManager
         {
             // 加载数据库连接驱动
             Class.forName(Config.DRIVER);
-            return DriverManager.getConnection(Config.url.toString(), Config.user, Config.password);
+            Connection connection = DriverManager.getConnection(Config.url.toString(), Config.user, Config.password);
+            connection.setAutoCommit(autoCommit);
+
+            return connection;
         }
         catch (ClassNotFoundException e)
         {
@@ -173,5 +199,49 @@ public final class DBManager
             System.out.println("创建连接失败！请检查地址或用户名密码的正确性以及网络是否可用！");
             return null;
         }
+    }
+
+    /**
+     * 获取当前事务的提交方式，默认是自动提交
+     *
+     * (get transaction state, default auto commit)
+     *
+     * @return 返回是否自动提交事务 (the state)
+     * */
+    public static boolean isAutoCommit()
+    {
+        return autoCommit;
+    }
+
+    /**
+     * 设置事务提交方式，默认是自动提交
+     *
+     * (get transaction state, default auto commit)
+     *
+     * @param autoCommit 提交方式 (if auto commit)
+     * */
+    public static void setAutoCommit(boolean autoCommit)
+    {
+        DBManager.autoCommit = autoCommit;
+    }
+
+    /**
+     * 将一个装着数据库信息的 Map 转换成对应的 orm 实体类对象
+     * 正确的使用方法是这个 Map 的 key 值必须和实体类的属性相同
+     *
+     * (change data in Map to an orm object)
+     * (the right way to use is that the key of this Map must be the same as the field of this object)
+     *
+     * @param data 装着数据的 Map，通常是通过 DBWorker.queryMaps 获得的
+     *             (this Map is filled with data, and made by DBWorker.queryMaps)
+     * @param clazz 这个实体类的类型，T 就是具体类型，比如 Book.class
+     *              (the type of this entity, T is real type, such as Book.class)
+     * @param <T>  T 就是具体类型，比如 Book.class
+     *             (T is real type, such as Book.class)
+     * @return 返回获得的对象 (return your object)
+     * */
+    public static <T> T getBean(Map<String, Object> data, Class<T> clazz)
+    {
+        return MySQL.getBeanByMap(data, clazz);
     }
 }
--- src/com/fish/EasyMysql/DBWorker.java
+++ src/com/fish/EasyMysql/DBWorker.java
@@ -1,5 +1,7 @@
 package com.fish.EasyMysql;
 
+import core.MySQL;
+
 import java.io.*;
 import java.sql.*;
 import java.util.ArrayList;
@@ -9,7 +11,9 @@ import java.util.Map;
 
 /**
  * 一些数据库常用操作函数
- * CURD, you know, just some jobs about database...
+ *
+ * (CURD, you know, just some jobs about database...)
+ *
  * @author Fish
  * */
 public final class DBWorker
@@ -19,42 +23,24 @@ public final class DBWorker
 
     /**
      * 获得数据库连接
-     * database's connection
+     *
+     * (database's connection)
      * */
     private static Connection connection = null;
 
     /**
      * 数据库预处理语句
-     * database's worker
+     *
+     * (database's worker)
      * */
     private static PreparedStatement ps = null;
 
     /**
-     * 计算传入的字段有多少个，替换成 “?” 占位符输出
-     * you don't need to know what it means... hahaha, find a Chinese dictionary to help you!
-     * @param oneColumn 传入的一行内容
-     * @return 返回占位符
-     * */
-    private static String getAttrs(String[] oneColumn)
-    {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < oneColumn.length; i++)
-        {
-            sb.append("?");
-
-            if (i != oneColumn.length - 1)
-            {
-                sb.append(", ");
-            }
-        }
-
-        return sb.toString();
-    }
-
-    /**
      * 从文件中读取 SQL 指令语句，返回成 String
-     * read sql from a file
-     * @return 返回得到的 SQL 语句
+     *
+     * (read sql from a file)
+     *
+     * @return 返回得到的 SQL 语句 (return sql)
      * */
     private static String getSqlOrder(File sqlFile)
     {
@@ -86,9 +72,10 @@ public final class DBWorker
 
     /**
      * 叫醒工人，开始干活！
-     * hey, man, wake up!
      * 连接到数据库
-     * connect to database
+     *
+     * (hey, man, wake up!)
+     * (connect to database)
      * */
     public static void wakeUp()
     {
@@ -97,12 +84,21 @@ public final class DBWorker
 
     /**
      * 干完活，不用他的话就让他睡觉休息一会吧！
-     * finish the jobs, let him have a break if you don't need him any more!
+     * 并且会提交事务，所以强烈建议使用这个方法代替直接的提交
      * 关闭连接，释放资源！建议在执行 DBManager.update(...) 之前先释放现有资源！
-     * close somethings and release resources! Invoke this method before invoking DBManager.update(...)!
+     *
+     * (finish the jobs, let him have a break if you don't need him any more!)
+     * (this method will commit transaction finally)
+     * (close somethings and release resources! Invoke this method before invoking DBManager.update(...)!)
      */
     public static void sleep()
     {
+        // 关闭前先提交事务！
+        if (!DBManager.isAutoCommit())
+        {
+            commit();
+        }
+
         try
         {
             if (ps != null)
@@ -123,15 +119,43 @@ public final class DBWorker
     }
 
     /**
-     * 执行较为复杂的数据库命令，比如建表，删除数据库，创建并执行事务等等
-     * exeute some complex works
-     * 命令语句由 sqlFile 文件传入
-     * sql will read from file automatically
-     * @param sqlFile 要被执行的 sql 语句集合文件，包含一堆数据库执行语句
-     *                a file containing many sql orders that you want it to do
-     * @return true 执行成功，false 执行失败
+     * 提交事务
+     *
+     * (commit transaction)
      * */
-    public static boolean work(File sqlFile)
+    public static void commit()
+    {
+        try
+        {
+            connection.commit();
+        }
+        catch (SQLException e)
+        {
+            System.out.println("提交事务失败！");
+
+            try
+            {
+                // 回滚事务！
+                connection.rollback();
+            }
+            catch (SQLException ee)
+            {
+                System.out.println("事务回滚失败！");
+            }
+        }
+    }
+
+    /**
+     * 执行较为复杂的数据库命令，比如建表，删除数据库，创建并执行事务等等
+     * 命令语句由 sqlFile 文件传入
+     * (exeute some complex works)
+     * (sql will read from file automatically)
+     *
+     * @param sqlFile 要被执行的 sql 语句集合文件，包含一堆数据库执行语句
+     *                (a file containing many sql orders that you want it to do)
+     * @return 影响的行数，如果返回 -1，表示出现异常 (effected rows, -1 means error!)
+     * */
+    public static int work(File sqlFile)
     {
         return work(getSqlOrder(sqlFile));
     }
@@ -139,74 +163,86 @@ public final class DBWorker
     /**
      * 执行较为复杂的数据库命令，比如建表，删除数据库，创建并执行事务等等
      * 如果新建一张已存在的表，会返回失败，但是不影响下面执行的语句，比如插入数据
-     * exeute some complex works
-     * if you create a table existed, then you got a failure
-     * But the next code will keep running, such as insert
-     * @param sql 传入要执行的 sql 语句
-     * @return true 执行成功，false 执行失败
+     * (exeute some complex works)
+     * (if you create a table existed, then you got a failure)
+     * (But the next code will keep running, such as insert)
+     *
+     * @param sql 传入要执行的 sql 语句 (give it what you need to do)
+     * @return 影响的行数，如果返回 -1，表示出现异常 (effected rows, -1 means error!)
      */
-    public static boolean work(String sql)
+    public static int work(String sql)
     {
+        // log...
+        log(sql);
+
         try
         {
             Statement s = connection.createStatement();
-            return s.execute(sql);
+            return s.executeUpdate(sql);
         }
         catch (SQLException e)
         {
             System.out.println("命令执行失败！请检查 SQL 语法！也有可能是这个工人累死了！（斜眼笑）");
-            return false;
+            return -1;
         }
     }
 
     /**
-     * 向数据库插入一行数据 (插入时间会出错！)
-     * insert some data
-     * @param tableName 要插入的表名 table name
-     * @param oneColumn 要插入的一行内容，包括主键，比如说 id
-     *                  示例：
-     *                  向 goods_cates 表插入一行数据，“8” 和 “手机” 是行内元素
-     *                  DBWorker.insert("goods_cates", new String[] {"8", "手机"});
-     *                  the data you want to insert
-     *                  the whole column data, including primary key like "id"!
-     * @return true 插入成功，false 插入失败
-     * */
-    public static boolean insert(String tableName, String[] oneColumn)
+     * 执行较为复杂的数据库命令，比如建表，删除数据库，创建并执行事务等等
+     * 如果新建一张已存在的表，会返回失败，但是不影响下面执行的语句，比如插入数据
+     * (exeute some complex works)
+     * (if you create a table existed, then you got a failure)
+     * (But the next code will keep running, such as insert)
+     *
+     * @param sql 传入要执行的 sql 语句 (give it what you need to do)
+     * @return 结果集 (result set)
+     */
+    private static ResultSet workQuery(String sql)
     {
-        StringBuilder sql = new StringBuilder("INSERT ");
-        sql.append(tableName);
-        sql.append(" VALUES(" + getAttrs(oneColumn) + ");");
+        // log...
+        log(sql);
 
         try
         {
-            ps = connection.prepareStatement(sql.toString());
-
-            for (int i = 1; i <= oneColumn.length; i++)
-            {
-                ps.setObject(i, oneColumn[i - 1]);
-            }
-
-            return ps.execute();
+            Statement s = connection.createStatement();
+            return s.executeQuery(sql);
         }
         catch (SQLException e)
         {
-            System.out.println("插入失败！请检查需要插入的内容是否正确！");
-            return false;
+            System.out.println("命令执行失败！请检查 SQL 语法！也有可能是这个工人累死了！（斜眼笑）");
+            return null;
         }
+    }
+
+    /**
+     * 插入表数据，根据传入对象的 @Table 和 @Column 值来插入 (插入时间会出错！)
+     * (insert data, depending on the value of @Table and @Column)
+     *
+     * @param data 插入对象，这必须是一个标准 javabean 对象，
+     *               在类上使用 @Table 注解，并传入对应的表名，而对应的列名则是通过 @Column 来获得
+     *               (orm object, this a standar javabean, also, it has @Table whose value is the table name,
+     *               and @Column with the column of this table)
+     * @return 影响的行数，如果返回 -1，表示出现异常 (effected rows, -1 means error!)
+     */
+    public static int insert(Object data)
+    {
+        return work(MySQL.insertSQL(data));
     }
 
     /**
      * 从数据库删除一行数据
-     * delete some data
-     * @param tableName 要删除内容的表名 table name
+     *
+     * (delete some data)
+     *
+     * @param tableName 要删除内容的表名 (table name)
      * @param selection 要删除的内容的筛选条件
      *                  比如，你要删除 id = 1 的元素，在这个地方就传入 "id = 1"；
      *                  也就是说：列名 = 列内容;
      *                  如果列内容是字符串，则需要使用 (\") 把字符串包住，比如 name = \"选择器\";
      *                  这里需要注意，如果传入 1，就会删除整份表，为了防止误操作导致删除整份表，在这里特意对 "1" 做了处理，
      *                  如果你要删除整份表，传入 "1" 将是无效的，需要传入 "ALL" 才能执行！
-     *                  delete data depending on your selection
-     * @return true 删除成功，false 删除失败
+     *                  (delete data depending on your selection)
+     * @return true 删除成功 (Done!)，false 删除失败 (you failed!)
      */
     public static boolean delete(String tableName, String selection)
     {
@@ -243,104 +279,60 @@ public final class DBWorker
     }
 
     /**
-     * 从数据库修改一行数据
-     * update some data
-     * @param tableName 要被修改数据的表名 table name
-     * @param columnName 要被修改的列名称 the column you want to update
-     * @param newValue 新设定的值 the new value you want to set
-     * @param oldValue 旧的值 the old value, which can find the data you want to update
-     *                 示例：
-     *                 从 goods_cates 表中修改一行数据，把 cate_name = "笔记本配件" 修改成 cate_name = "手机"
-     *                 int count = DBWorker.update("goods_cates", "cate_name", "笔记本配件", "手机");
-     *                 System.out.println(count); // 打印出修改的行数
-     * @return 修改影响的行数 the rows effected
+     * 更新表数据，根据传入对象的 @Table 和 @Column 值来更新
+     *
+     * (update data, depending on the value of @Table and @Column)
+     *
+     * @param oldData 旧的对象，这必须是一个标准 javabean 对象，
+     *               在类上使用 @Table 注解，并传入对应的表名，而对应的列名则是通过 @Column 来获得，
+     *               并且这个对象必须进行赋值，否则无法找到对应的数据，有可能导致整张表被覆盖
+     *               (old object, this a standar javabean, also, it has @Table whose value is the table name,
+     *               and @Column with the column of this table, and its field must be not null!)
+     * @param newData 新的对象，这必须是一个标准 javabean 对象，
+     *               在类上使用 @Table 注解，并传入对应的表名，而对应的列名则是通过 @Column 来获得
+     *               (old object, this a standar javabean, also, it has @Table whose value is the table name,
+     *               and @Column with the column of this table)
+     * @return 影响的行数，如果返回 -1，表示出现异常 (effected rows, -1 means error!)
      */
-    public static int update(String tableName, String columnName, String oldValue, String newValue)
+    public static int update(Object oldData, Object newData)
     {
-        StringBuilder sql = new StringBuilder("UPDATE ");
-        sql.append(tableName);
-        sql.append(" SET " + columnName + " = \"" + newValue + "\"");
-        sql.append(" WHERE " + columnName + " = \"" + oldValue + "\";");
-
-        try
-        {
-            ps = connection.prepareStatement(sql.toString());
-
-            return ps.executeUpdate();
-        }
-        catch (SQLException e)
-        {
-            System.out.println("更新数据失败！请检查需要更新的行内容是否正确！");
-            return 0;
-        }
+        return work(MySQL.updateSQL(oldData, newData));
     }
 
     /**
      * 向数据库查询数据
-     * query some data
+     *
+     * (query some data)
+     *
      * @param tableName 要查询内容的表名 table name
      * @param selection 要查询的内容的筛选条件
      *                  比如，你要查询 id = 1 的元素，在这个地方就传入 "id = 1"；
      *                  也就是说：列名 = 列内容;
      *                  如果列内容是字符串，则需要使用 (\") 把字符串包住，比如 name = \"选择器\";
      *                  这里需要注意，如果传入 "1" 或 "ALL"，就会查询整份表，把整份表包装到字符串中
-     *                  query data depending on your selection
-     * @return 返回查询到的结果，字符串封装
+     *                  (query data depending on your selection)
+     * @return 返回查询到的结果，字符串封装 (return strings that filled with data)
      */
     public static List<String> queryStrings(String tableName, String selection)
     {
         List<String> result = new ArrayList<>();
-        StringBuilder sql = new StringBuilder("SELECT * FROM ");
-        sql.append(tableName);
+        List<Map> maps = queryMaps(tableName, selection);
 
-        if ("ALL".equalsIgnoreCase(selection))
+        for (int i = 0; i < maps.size(); i++)
         {
-            selection = "1";
+            result.add(maps.get(i).toString());
         }
 
-        sql.append(" WHERE " + selection + ";");
-
-        try
-        {
-            // 计算列数
-            ps = connection.prepareStatement("DESC " + tableName);
-            int columns = ps.executeUpdate();
-
-            ps = connection.prepareStatement(sql.toString());
-
-            StringBuilder sb = null;
-            ResultSet rs = ps.executeQuery();
-            ResultSetMetaData rsmd = rs.getMetaData();
-            while (rs.next())
-            {
-                sb = new StringBuilder();
-                for (int i = 1; i <= columns; i++)
-                {
-                    sb.append("[" + rsmd.getColumnName(i) + ": " + rs.getObject(i) + "]");
-
-                    if (i != columns)
-                    {
-                        sb.append(", ");
-                    }
-                }
-
-                result.add(sb.toString());
-            }
-
-            return result;
-        }
-        catch (SQLException e)
-        {
-            System.out.println("查询失败！请检查传入的选择器是否有效！");
-            return null;
-        }
+        return result;
     }
 
     /**
      * 向数据库查询数据表
-     * query some data
-     * @param tableName 要查询内容的表名 table name
-     * @return 返回查询到的结果，字符串封装
+     *
+     * (query some data)
+     *
+     * @param tableName 要查询内容的表名 (table name)
+     * @return 返回查询到的结果，字符串封装 (return strings that filled with data)
      */
     public static List<String> queryStrings(String tableName)
     {
@@ -349,16 +341,18 @@ public final class DBWorker
 
     /**
      * 向数据库查询数据
-     * query some data
      *
-     * @param tableName 要查询内容的表名 table name
+     * (query some data)
+     *
+     * @param tableName 要查询内容的表名 (table name)
      * @param selection 要查询的内容的筛选条件
      *                  比如，你要查询 id = 1 的元素，在这个地方就传入 "id = 1"；
      *                  也就是说：列名 = 列内容;
      *                  如果列内容是字符串，则需要使用 (\") 把字符串包住，比如 name = \"选择器\";
      *                  这里需要注意，如果传入 "1" 或 "ALL"，就会查询整份表，把整份表包装到字符串中
-     *                  query data depending on your selection
-     * @return 返回查询到的结果，HashMap 封装，可通过键值获取到具体的值，键值就是表的列名(the key is the column of this table)
+     *                  (query data depending on your selection)
+     * @return 返回查询到的结果，HashMap 封装，可通过键值获取到具体的值，键值就是表的列名
+     *         (the key is the column of this table)
      */
     public static List<Map> queryMaps(String tableName, String selection)
     {
@@ -375,15 +369,18 @@ public final class DBWorker
 
         try
         {
-            // 计算列数
-            ps = connection.prepareStatement("DESC " + tableName);
-            int columns = ps.executeUpdate();
+            // log...
+            log(sql.toString());
 
             ps = connection.prepareStatement(sql.toString());
 
-            Map<String, Object> map = null;
             ResultSet rs = ps.executeQuery();
+            Map<String, Object> map = null;
             ResultSetMetaData rsmd = rs.getMetaData();
+
+            // 计算列数
+            int columns = rsmd.getColumnCount();
+
             while (rs.next())
             {
                 map = new HashMap<>();
@@ -406,10 +403,12 @@ public final class DBWorker
 
     /**
      * 向数据库查询数据表
-     * query some data
      *
-     * @param tableName 要查询内容的表名 table name
-     * @return 返回查询到的结果，HashMap 封装，可通过键值获取到具体的值，键值就是表的列名(the key is the column of this table)
+     * (query some data)
+     *
+     * @param tableName 要查询内容的表名 (table name)
+     * @return 返回查询到的结果，HashMap 封装，可通过键值获取到具体的值，键值就是表的列名
+     *         (the key is the column of this table)
      */
     public static List<Map> queryMaps(String tableName)
     {
@@ -417,12 +416,68 @@ public final class DBWorker
     }
 
     /**
+     * 获取表数据，根据传入对象的 @Table 和 @Column 值来查询
+     *
+     * (query data, depending on the value of @Table and @Column)
+     *
+     * @param data 查询依据对象，这必须是一个标准 javabean 对象，
+     *               在类上使用 @Table 注解，并传入对应的表名，而对应的列名则是通过 @Column 来获得
+     *               (orm object, this a standar javabean, also, it has @Table whose value is the table name,
+     *               and @Column with the column of this table)
+     * @param clazz 这个实体类的类型，T 就是具体类型，比如 Book.class
+     *              (the type of this entity, T is real type, such as Book.class)
+     * @param <T> T 就是具体类型，比如 Book.class
+     *            (T is real type, such as Book.class)
+     * @return 返回实体对象 (return a entity object)
+     */
+    public static <T> T query(Object data, Class<T> clazz)
+    {
+        ResultSet rs = workQuery(MySQL.querySQL(data));
+        Map<String, Object> map = null;
+
+        if (rs != null)
+        {
+            try
+            {
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columns = rsmd.getColumnCount();
+                while (rs.next())
+                {
+                    map = new HashMap<>();
+                    for (int i = 1; i <= columns; i++)
+                    {
+                        map.put(rsmd.getColumnName(i), rs.getObject(i));
+                    }
+                }
+            }
+            catch (SQLException e)
+            {
+                System.out.println("封装对象失败！");
+            }
+        }
+
+        return DBManager.getBean(map, clazz);
+    }
+
+    /**
+     * 输出日志，暂时只能显示 sql 语句
+     *
+     * (output the sql)
+     * */
+    private static void log(String sql)
+    {
+        System.err.println("(log ===> sql : " + sql + ")");
+    }
+
+    /**
      * 将查询到的一整个表保存进文件中
-     * save the whole table into a file
-     * @param tableName 要查询的一整份表 table name
+     *
+     * (save the whole table into a file)
+     *
+     * @param tableName 要查询的一整份表 (table name)
      * @param filePath 文件保存路径，包括文件名，合法的取值：Z:/table.txt
-     *                 saving path, including file name, like "Z:/table.txt"
-     * @return true 保存成功，false 保存失败
+     *                 (saving path, including file name, like "Z:/table.txt")
+     * @return true 保存成功 (Done!)，false 保存失败 (you failed!)
      * */
     public static boolean putTableInFile(String tableName, String filePath)
     {
